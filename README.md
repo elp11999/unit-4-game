4d3ef4406d9e4f29d68082f258adb5c8dbc3dc88
Michael Henderson <mhenderson557@yahoo.com>
2018-12-21 16:21:27 -0600

    Switch Game to StarWars.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Crystal guess game
+# Star Wars game
 
-* This project is a Web based "Number Guess" game application.
+* This project is a Web based "Star Wars" game application.
 * This project was a perfect way for me to get introduced to JQuery.
 * To get started, clone the repository and check out the code.
 * To just run the application, use link https://elp11999.github.io/unit-4-game/
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,108 +1,309 @@
 //
-// JavaScript for Unit-4-test
+// JavaScript for Stars Wars
 //
 $(document).ready(function() {
-    console.log("Cyrstal game started...");
 
-    var crystalGame = {
-        totalScore: 0,
-        totalWins: 0,
-        totalLoses: 0,
-        randomNumber: 0,        
-        crystalValues: [],
+    console.log("Star Wars game started...");
+
+    var player1 = {
+        name: "Obi-Wan Kenobi",
+        healthPoints: 0,
+        baseAttackPower: 0,
+        attackPower: 0,
+        couterAttackPower: 0
+    };
+    
+    var player2 = {
+        name: "Darth Sideous",
+        healthPoints: 0,
+        baseAttackPower: 0,
+        attackPower: 0,
+        couterAttackPower: 0
+    };
+    
+    var player3 = {
+        name: "Luke Skywalker",
+        healthPoints: 0,
+        baseAttackPower: 0,
+        attackPower: 0,
+        couterAttackPower: 0
+    };
+    
+    var player4 = {
+        name: "Darth Maul",
+        healthPoints: 0,
+        baseAttackPower: 0,
+        attackPower: 0,
+        couterAttackPower: 0
+    };
+    
+    var StarWarsGame = {
+        gameInProgress: false,
+        attackInProgress: false,
+        enemiesLeft: 0,
+        players : ["", player1, player2, player3, player4],
+        activePlayerID: 0,
+        activeDefenderID: 0,
+        activeBackgroundID: 0,
+        activeTimerID: null,
+        backgroundImages: ["assets/images/starwars1.jpg", "assets/images/starwars2.jpg", "assets/images/starwars3.jpg", "assets/images/starwars4.jpg", "assets/images/starwars5.jpg"],
         audioElement: "",
         winnerSound: "assets/audio/winner.wav",
         loserSound: "assets/audio/loser.wav",
-        startGame: function() {
-            var crystalValue = 0;
-
-            // Reset game total 
-            this.crystalValues = [];
-            this.totalScore = 0;
-            $(".totalscore").text(this.totalScore);
-
-            // Create random number between 19 - 120
-            this.randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-            $(".randomnumber").text(this.randomNumber);
-            //console.log("startGame: randomNumber=" + this.randomNumber);
-
-            // Create random numbers for crystals between 1 - 12;
-            for (var i = 1; i <= 4; i++) {
-                this.crystalValues[i] = this.createcrystalValue();
-                //console.log("startGame: crystalValue=" + this.crystalValues[i]);
-                $(".crystal" + i).data("crystalvalue", this.crystalValues[i]);
+        // Show only player selected
+        showPlayerOnly : function(id) {
+            for (var index = 1; index < this.players.length; index++) {
+                if (index === id)
+                    $("#player" + index).show();
+                else
+                    $("#player" + index).hide();
             }
         },
-        createcrystalValue: function() {
-            var index = 0;
-            var crystalValue = 0;
 
-            // Create a unique random crystal value
-            while (true) {
-                crystalValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-                index = this.crystalValues.indexOf(crystalValue);
-                if (index < 0)
-                    break;
+        // Show all players but the player picked
+        showEnemiesBut : function(id) {
+            for (var index = 1; index < this.players.length; index++) {
+                if (index === id)
+                    $("#enemy" + index).hide();
+                else {
+                    $(".enemy" + index + "health").text(this.players[index].healthPoints);
+                    $("#enemy" + index).show();
+                }
             }
-
-            // Return crystal value
-            return crystalValue;        
         },
-        playGame: function(value) {
 
-            // Increment total score based on crystal value
-            this.totalScore += value;
-            $(".totalscore").text(this.totalScore);
-            //console.log("playGame: totalScore=" + this.totalScore);
+        // Show all players
+        showAllPlayers : function() {
+            for (var index = 1; index < this.players.length; index++) {
+                $("#player" + index).show();
+                $(".player" + index + "health").text(this.players[index].healthPoints);
+            }
+        },
 
-            // Hide win lose message
-            $(".result").hide();   
+        // Hide all enemies
+        hideAllEnemies : function() {
+            $(".enemies").hide();
+            for (var index = 1; index < this.players.length; index++) {
+                $("#enemy" + index).hide();
+            }
+        },
 
-            // Check win or lose
-            if (this.totalScore === this.randomNumber) {
+        // Hide all defenders
+        hideAllDefenders : function() {
+            $(".defenders").hide();
+            for (var index = 1; index < this.players.length; index++) {
+                $("#defender" + index).hide();
+            }
+        },
 
-                // You won
-                this.totalWins++;
-                $(".wins").text(this.totalWins);
+        // Initialize Game
+        initalizeGame: function() {
 
-                $(".result").text("You Win!!");
-                $(".result").show();
+            // Setup flags
+            this.gameInProgress = false;
+            this.attackInProgress = false;            
 
-                this.audioElement.setAttribute("src", this.winnerSound);
-                this.audioElement.play();
+            // Start background image rotation timer
+            if (this.activeTimerID === null)
+                this.activeTimerID = setInterval(this.changeBackgroundImage, 5000);
 
-                // Restart game
-                crystalGame.startGame();
-            } else if (this.totalScore > this.randomNumber) {
+            // Initalize all  players
+            this.initalizePlayers();
 
-                // You lost
-                this.totalLoses++;
-                $(".loses").text(this.totalLoses);
+            // Show all players
+            this.showAllPlayers();
+        
+            // Hide all enemies
+            this.hideAllEnemies();
+        
+            // Hide all defenders
+            this.hideAllDefenders();
 
-                $(".result").text("You Lose!!");
-                $(".result").show();
+            // Clear out results area
+            $(".gameResult1").text("");
+            $(".gameResult2").text("");
 
+        },
+
+        // Initialize Players
+        initalizePlayers: function() {
+
+            // Set values for Health points, etc... for each player
+            for (var i = 0; i < this.players.length; i++) {
+                var player = this.players[i];
+                player.healthPoints = Math.floor(Math.random() * (150 - 100 + 1) + 100);
+                player.baseAttackPower = Math.floor(Math.random() * (15 - 5 + 1) + 5);
+                player.attackPower = 0;
+                player.couterAttackPower = Math.floor(Math.random() * (30 - 5 + 1) + 5);
+            }
+        },
+
+        // Pick the player
+        pickPlayer: function(id) {
+
+            // Set active player
+            this.activePlayerID = parseInt(id);
+            this.showPlayerOnly(this.activePlayerID);            
+            console.log("activePlayerID=" + this.activePlayerID);
+
+            // Show the enemies
+            $(".enemies").css("display", "inline-block");
+            $(".enemies").show();
+            this.showEnemiesBut(this.activePlayerID);
+            this.enemiesLeft = 3;
+            
+            // Hide all defenders
+            this.hideAllDefenders();
+        },
+
+        // Pick the enemy
+        pickEnemy: function(id) {
+    
+            // Hide selected enemy
+            $("#enemy" + id).hide();    
+            if (StarWarsGame.enemiesLeft === 1)
+                $(".enemies").hide();
+        
+            // Set active defender
+            this.activeDefenderID = parseInt(id);
+            console.log("activeDefenderID=" + this.activeDefenderID);
+            
+            // Set defender health points
+            $(".defender" + id + "health").text(this.players[this.activeDefenderID].healthPoints);
+
+            // Show defender
+            $(".defenders").css("display", "inline-block");
+            $(".defenders").show();
+            $("#defender" + id).show();
+            
+            // Clear game result text areas
+            $(".gameResult1").text("");
+            $(".gameResult2").text("");
+    
+            // Set game in progress
+            this.gameInProgress = true;
+            this.attackInProgress = true;
+
+        },
+
+        // Play the game "ATTACK"
+        attack: function() {
+
+            // Get the player and defender
+            var player = this.players[this.activePlayerID];
+            var defender = this.players[this.activeDefenderID];
+            //console.log(player);
+            //console.log(defender);
+            
+            // Update players attack power
+            player.attackPower += player.baseAttackPower;
+
+            // Update defenders health points
+            defender.healthPoints -= player.attackPower;
+            $(".defender" + this.activeDefenderID + "health").text(defender.healthPoints);
+
+            // Clear game result text areas
+            $(".gameResult1").text("");
+            $(".gameResult2").text("");
+            
+            // Check if the player beat the defender
+            if (defender.healthPoints <= 0) {
+                this.attackInProgress = false;
+                this.enemiesLeft--;
+                if (this.enemiesLeft > 0) {
+                    $("#defender" + this.activeDefenderID).hide();
+                    var result = "You have defeated " + defender.name + ", choose another enemy to fight.";
+                    $(".gameResult1").text(result);
+                } else {
+                    $("#defender" + this.activeDefenderID).hide();
+                    $(".gameResult1").text("You Won!!! GAME OVER!!!");
+                    $("#replay").show();
+                    this.audioElement.setAttribute("src", this.winnerSound);
+                    this.audioElement.play();
+                }
+                return;
+            }
+            
+            // Update players health points
+            player.healthPoints -= defender.couterAttackPower;            
+            $(".player" + this.activePlayerID + "health").text(player.healthPoints);
+
+            // Check if the player lost the game
+            if (player.healthPoints <= 0) {
+                    this.attackInProgress = false;
+                $(".gameResult1").text("You have been defeated... GAME OVER!!!!");
+                $("#replay").show();  
                 this.audioElement.setAttribute("src", this.loserSound);
                 this.audioElement.play();
+            } else {
+                var attack = "You attacked " + defender.name + " with " + player.attackPower + " damage points.";
+                var counterAttack = defender.name + " attacked you back with " + defender.couterAttackPower + " points.";
+                $(".gameResult1").text(attack);
+                $(".gameResult2").text(counterAttack);
+            }
+        },
 
-                // Restart game
-                crystalGame.startGame();
-            }       
+        // Rotate background image
+        changeBackgroundImage: function() {
+            $("body").css("background", "url(" + StarWarsGame.backgroundImages[StarWarsGame.activeBackgroundID] + ") no-repeat center center fixed");
+            $("body").css("background-size", "cover");
+            StarWarsGame.activeBackgroundID++;
+            if (StarWarsGame.activeBackgroundID >= StarWarsGame.backgroundImages.length)
+                StarWarsGame.activeBackgroundID = 0;
         }
+
     };
-    
-    // Create audio element
-    crystalGame.audioElement = document.createElement("audio");
 
-    // Register click event handler for the crystal images
-    $(".crystal").click(function(event) {
-        var crystalValue;
+    // Set callback when a player is selected
+    $('.playerimage').click(function(event) {
 
-        crystalValue = $(this).data("crystalvalue");
-        crystalGame.playGame(parseInt(crystalValue));
-        //console.log("crystal value=" + crystalValue);
+        // Check if game is in-progess
+        if (StarWarsGame.gameInProgress === true)
+            return;
+
+        // Stop backgound image rotation
+        if (StarWarsGame.activeTimerID != null) {
+            clearInterval(StarWarsGame.activeTimerID);
+            StarWarsGame.activeTimerID = null;
+        }
+
+        // Get player id
+        var id = $(this).data("playerindex"); 
+
+        // Pick the player
+        StarWarsGame.pickPlayer(id);
     });
 
-    // Start game
-    crystalGame.startGame();
+    // Set callback when an enemy is selected
+    $('.enemyimage').click(function(event) {
+
+        // Check if game is in progress
+        if (StarWarsGame.attackInProgress === true)
+            return;
+
+        // Get player id
+        var id = $(this).data("playerindex");
+
+        // Pick the enemy
+        StarWarsGame.pickEnemy(id);
+    });
+
+    // Set callback when the attack button is clicked
+    $("#attack").click(function(event) {
+        if (StarWarsGame.attackInProgress === true)
+            StarWarsGame.attack();
+    });
+
+    // Set callback when the replay button is clicked
+    $("#replay").click(function(event) {
+        StarWarsGame.initalizeGame();
+        $("#replay").hide();
+    });
+
+    
+    // Create audio element
+    StarWarsGame.audioElement = document.createElement("audio");
+
+    // Initalize the game
+    StarWarsGame.initalizeGame();
 });
